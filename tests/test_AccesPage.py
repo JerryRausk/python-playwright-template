f18b094e531ee00cafab57141b716bfd752c0676
Jerry Rausk <Jerry.Rausk@gmail.com>
2023-06-12 22:49:50 +0200

    print secrets

--- tests/test_AccesPage.py
+++ tests/test_AccesPage.py
@@ -21,4 +21,5 @@ def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_in
 
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*intro"))
-    expect(page).to_have_title(os.environ["USERNAME_HERE"])
+    print(os.environ["USERNAME_HERE"])
+    print(os.environ["PASSWORD_HERE"])
