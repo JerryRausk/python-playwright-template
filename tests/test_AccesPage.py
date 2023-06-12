c5e2da56debc17f9972e534bb5bb61a991a79506
Jerry Rausk <Jerry.Rausk@gmail.com>
2023-06-12 22:37:49 +0200

    broken cron

--- tests/test_AccesPage.py
+++ tests/test_AccesPage.py
@@ -21,5 +21,4 @@ def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_in
 
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*intro"))
-    expect(os.environ["USERNAME_HERE"] == "Hello?")
-    expect(os.environ["PASSWORD_HERE"] == "pwd?")
+    expect(page).to_have_title(os.environ["USERNAME_HERE"])
